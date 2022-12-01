89568178efeb4738ec46ba8e8e8f44e608e8f919
Ka Shun Kei <kkei@firstderivatives.com>
2022-12-01 15:55:04 +0800

    Set up Sublime

---
+++ config.bat
@@ -0,0 +1,12 @@
+SET PATH=%PATH%;C:\IDE\Sublime
+SET PATH=%PATH%;C:\Programming_Language\jdk\jdk-19.0.1\bin
+SET PATH=%PATH%;C:\Programming_Language\q\w64
+SET PATH=%PATH%;C:\Programming_Language\R\R-4.2.2\bin
+SET PATH=%PATH%;C:\Programming_Language\R\R-4.2.2\bin\x64
+
+
+
+SET JAVA_HOME=C:\Programming_Language\jdk-19.0.1\bin
+SET QHOME=C:\Programming_Language\q
+SET QLIC=C:\Programming_Language\q
+SET btsrc=C:\kdb_package\btick\src
--- main.q
+++ main.q
@@ -4,3 +4,4 @@
 basic_transform[10]
 polar_transform[10000]
 show .z.i;
+
